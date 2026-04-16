4dabb71734872f19110ae4bc5badd53a66f19ea8
Abhakumar39 <Abhay.k7417@gmail.com>
2026-04-16 20:09:49 +0530

    first program

--- Udemy Question/_1_First_Java_P.java
+++ Udemy Question/_1_First_Java_P.java
@@ -1,5 +1,7 @@
 class _1_First_Java_P {
     public static void main(String[] args) {
         System.out.println("Hello Wold");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
     }
 }
